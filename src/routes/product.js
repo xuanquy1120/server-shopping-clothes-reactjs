77c912339ff2f600a0d3cf3728e8e8ef274a6dbb
xuanquy1120 <xuanquy1120@gmail.com>
2022-08-03 13:42:14 +0700

    fix bug

--- src/routes/product.js
+++ src/routes/product.js
@@ -5,7 +5,11 @@ import { authMiddleware } from "middleware/auth.middleware";
 const router = Router();
 
 router.get("/", productController.getProduct);
-router.post("/add", authMiddleware.isAuthenticated,productController.addProduct);
-router.get("/findProduct",productController.findProduct);
+router.post(
+  "/add",
+  authMiddleware.isAuthenticated,
+  productController.addProduct
+);
+router.get("/findProduct", productController.findProduct);
 
 export default router;
